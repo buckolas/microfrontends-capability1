f01f85f9078727548a15e8c39316bb9d43976114
Michael Prendergast <mikeprendergast@gmail.com>
2020-04-13 15:34:04 -0400

    Successful attemnpt with single-spa and SystemJS

---
+++ extra-webpack.config.js
@@ -0,0 +1,13 @@
+const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default
+
+module.exports = (angularWebpackConfig, options) => {
+  const singleSpaWebpackConfig = singleSpaAngularWebpack(angularWebpackConfig, options)
+
+  // singleSpaWebpackConfig.output = {
+  //   library: "capability1App",
+  //   libraryTarget: "window"
+  // };
+
+  // Feel free to modify this webpack config however you'd like to
+  return singleSpaWebpackConfig
+}
